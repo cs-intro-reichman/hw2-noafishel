99c88e6ab967bd23b06fc73837435372e852937a
Noa Fishel <noa.fishel@post.runi.ac.il>
2025-11-09 01:16:19 +0200

    CalcPi completed

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,17 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int num = Integer.parseInt(args[0]);
+		double divisor = 1;
+		double sum = 0;
+		int i = 0;
+		while (i < num) {
+			divisor = (i % 2 == 0 ? 1 : -1) * (1 + 2 * i); 
+			sum += 1 / divisor;
+			i++;
+		}
+		double pi = sum * 4;
+		System.out.println("pi according to java: " + Math.PI);
+		System.out.println("pi, approximated:     " + pi);
 	}
 }

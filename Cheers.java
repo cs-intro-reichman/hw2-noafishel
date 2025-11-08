f01bb02616be42da50f789ffdc6c0407b6140bb5
Noa Fishel <noa.fishel@post.runi.ac.il>
2025-11-08 02:31:37 +0200

    Cheers completed

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,21 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String str = args[0];
+            int num = Integer.parseInt(args[1]);
+            String upperCaseStr = str.toUpperCase();
+            for (int i = 0; i < str.length(); i++){
+                if ("AEFHILMNORSX".indexOf(upperCaseStr.charAt(i)) != -1) {
+                        System.out.println("Give me an " + upperCaseStr.charAt(i) +
+                                           ": " + upperCaseStr.charAt(i) + "!");
+                } else {
+                        System.out.println("Give me a  " + upperCaseStr.charAt(i) +
+                                           ": " + upperCaseStr.charAt(i) + "!");
+                }
+            }
+            System.out.println("What does that spell?");
+            for (int y = 0; y < num; y++){
+                System.out.println(upperCaseStr + "!!!");
+            }
         }
 }
